77c9dc62688f1650ac46dc3638a0323a7b9d2bbd
Surya A <surya.a@microfocus.com>
2022-07-03 14:50:43 +0530

    adding spring boot config and h2 database

---
+++ src/main/java/springBoot/DataBaseSpringBootDemo.java
@@ -0,0 +1,12 @@
+package springBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataBaseSpringBootDemo {
+
+    public static void main(String[] args){
+        SpringApplication.run(DataBaseSpringBootDemo.class, args);
+    }
+}
