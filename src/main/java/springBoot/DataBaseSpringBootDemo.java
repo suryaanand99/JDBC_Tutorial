ef9d18035e335ac06b11f51e69e2ee924f92e509
Surya A <surya.a@microfocus.com>
2022-07-03 15:19:04 +0530

    sample example of Spring JDBC

--- src/main/java/springBoot/DataBaseSpringBootDemo.java
+++ src/main/java/springBoot/DataBaseSpringBootDemo.java
@@ -1,12 +1,26 @@
 package springBoot;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springBoot.jdbc.PersonJDBCDAO;
 
 @SpringBootApplication
-public class DataBaseSpringBootDemo {
+public class DataBaseSpringBootDemo implements CommandLineRunner {
+    private Logger LOGGER = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    PersonJDBCDAO personJDBCDAO;
 
     public static void main(String[] args){
         SpringApplication.run(DataBaseSpringBootDemo.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        LOGGER.info("All persons -> {}", personJDBCDAO.findAll());
     }
 }
---
+++ src/main/java/springBoot/jdbc/PersonJDBCDAO.java
@@ -0,0 +1,20 @@
+package springBoot.jdbc;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+import springBoot.jdbc.entity.Person;
+
+import java.util.List;
+
+@Repository
+public class PersonJDBCDAO {
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    public List<Person> findAll() {
+        return jdbcTemplate.query("select * from person", new BeanPropertyRowMapper<Person>(Person.class));
+    }
+}
