a354be4b2130942012249bf3eae595b2bd83d11e
Surya A <surya.a@microfocus.com>
2022-07-09 17:20:25 +0530

    sample 3 example of Spring JDBC

--- src/main/java/springBoot/DataBaseSpringBootDemo.java
+++ src/main/java/springBoot/DataBaseSpringBootDemo.java
@@ -7,6 +7,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import springBoot.jdbc.PersonJDBCDAO;
+import springBoot.jdbc.entity.Person;
+
+import java.util.Date;
 
 @SpringBootApplication
 public class DataBaseSpringBootDemo implements CommandLineRunner {
@@ -23,5 +26,10 @@ public class DataBaseSpringBootDemo implements CommandLineRunner {
     public void run(String... args) throws Exception {
         LOGGER.info("All persons -> {}", personJDBCDAO.findAll());
         LOGGER.info("Person -> {}", personJDBCDAO.findById(10001));
+        LOGGER.info("Delete Person 10002 -> no of rows affected {}", personJDBCDAO.deleteById(10002));
+        LOGGER.info("update Person 10001 -> no of rows affected {}",
+                personJDBCDAO.update(new Person(10001, "Ranga", "Bartley", new Date())));
+        LOGGER.info("insert Person -> no of rows affected {}",
+                personJDBCDAO.insert(new Person(10004, "Sara", "Hyderabad", new Date())));
     }
 }
--- src/main/java/springBoot/jdbc/PersonJDBCDAO.java
+++ src/main/java/springBoot/jdbc/PersonJDBCDAO.java
@@ -6,6 +6,7 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 import springBoot.jdbc.entity.Person;
 
+import java.sql.Timestamp;
 import java.util.List;
 
 @Repository
@@ -22,5 +23,34 @@ public class PersonJDBCDAO {
         return jdbcTemplate.queryForObject(
                 "select * from person where id=?", new Object[]{id},
                 new BeanPropertyRowMapper<Person>(Person.class));
+    }
+
+    public int deleteById(int id) {
+        return jdbcTemplate.update(
+                "delete from person where id=?", new Object[]{id});
+    }
+
+    public int insert(Person person) {
+        return jdbcTemplate.update(
+                "insert into person (id, name, location, birth_date )" +
+                        " values(?, ?, ?, ?)",
+                new Object[]{
+                        person.getId(),
+                        person.getName(),
+                        person.getLocation(),
+                        new Timestamp(person.getBirthDate().getTime())
+                });
+    }
+
+    public int update(Person person) {
+        return jdbcTemplate.update(
+                "update person set name = ?, location =?, birth_date = ?" +
+                        " where id = ?",
+                new Object[]{
+                        person.getName(),
+                        person.getLocation(),
+                        new Timestamp(person.getBirthDate().getTime()),
+                        person.getId(),
+                });
     }
 }
